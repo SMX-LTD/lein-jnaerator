ea8e3530fed7826d9649389f2a1ae2b11564fa8e
Abhishek Reddy <abhishek.reddy@smxemail.com>
2014-04-03 15:07:14 +1300

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.smxemail/lein-jnaerator "0.1.0-SNAPSHOT"
+(defproject com.smxemail/lein-jnaerator "0.1.0"
   :description "Super thin wrapper around JNAerator CLI, allowing Leiningen projects to use it in a managed way."
   :url "https://github.com/SMX-LTD/lein-jnaerator"
   :license {:name "Eclipse Public License"
